52652850d0662f0864189f3f2df3d97ba6a80e65
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-11-16 21:13:56 -0500

    styling

--- src/Author.tsx
+++ src/Author.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react'
 import {IStringArr} from './utils' 
-
+import './Author.css'
 const Author =({author}:IStringArr)=>{
     useEffect(()=>{
       fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${author}`).then(res=>res.json()).then(res=> console.log(res))
     },[author])
     return(
       <aside>
-        <h2>{author}</h2>
+        <h2> -{author}</h2>
       </aside>
     )
   }
