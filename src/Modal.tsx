b93f52c6e56a7c3596a5451007930ced23e2a3b4
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-11-17 13:53:29 -0500

    closing with esc

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,13 +1,24 @@
 import './Modal.css'
-import {PropsWithChildren, ReactNode} from 'react';
+import {PropsWithChildren, ReactNode, useEffect, useRef} from 'react';
 
 type IModal = {
   close:()=>void;
   children:ReactNode;
 }
 const Modal = ({children, close}:PropsWithChildren<IModal>) => {
+  const modalRef=useRef(null);
+useEffect(()=>{
+  if(modalRef.current !==null){
+    modalRef.current.focus();
+  }
+},[modalRef])
+  const checkForEsc =(e)=>{
+    if(e.keyCode ===27){
+      close();
+    }
+  }
 return(
-  <div className="modal">
+  <div ref={modalRef} tabIndex={1} className="modal" onKeyDown={checkForEsc}>
     <button onClick={close}>x</button>
     {children}
   </div>
