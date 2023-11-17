0cba15884c8a3460ef7766cbf0cd3e9a10ac95ed
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-11-17 14:01:17 -0500

    types

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,25 +1,26 @@
 import './Modal.css'
-import {PropsWithChildren, ReactNode, useEffect, useRef} from 'react';
+import {PropsWithChildren, ReactNode, useEffect, useRef, KeyboardEvent} from 'react';
 
 type IModal = {
   close:()=>void;
   children:ReactNode;
 }
 const Modal = ({children, close}:PropsWithChildren<IModal>) => {
-  const modalRef=useRef(null);
+  const modalRef=useRef<HTMLInputElement>(null);
+
 useEffect(()=>{
   if(modalRef.current !==null){
     modalRef.current.focus();
   }
 },[modalRef])
-  const checkForEsc =(e)=>{
+  const checkForEsc =(e:KeyboardEvent<HTMLDivElement>)=>{
     if(e.keyCode ===27){
       close();
     }
   }
 return(
   <div ref={modalRef} tabIndex={1} className="modal" onKeyDown={checkForEsc}>
-    <button onClick={close}>x</button>
+    <button onClick={close} tabIndex={1}>x</button>
     {children}
   </div>
 )
